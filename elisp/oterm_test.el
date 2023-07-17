b43a73f8322589731181a98a7a86f5fbc9fe7f4c
Stephane Zermatten <szermatt@gmx.net>
2023-07-17 21:18:41 +0300

    oterm: test scrolling

--- elisp/oterm_test.el
+++ elisp/oterm_test.el
@@ -4,12 +4,13 @@
 (require 'ert)
 (require 'ert-x)
 
+(defconst oterm-test-prompt "$ ")
 (defmacro with-oterm-buffer (&rest body)
   `(ert-with-test-buffer ()
-     (oterm--exec "bash" "--noprofile" "--norc")
+     (oterm--exec "/usr/local/bin/bash" "--noprofile" "--norc")
      (while (eq (point-min) (point-max))
        (accept-process-output oterm-term-proc 0 100 t))
-     (oterm-send-raw-string "PS1='$ '\n")
+     (oterm-send-raw-string (concat "PS1='" oterm-test-prompt "'\n"))
      (oterm-wait-for-output)
      (narrow-to-region (- (point) 2) (point-max))
      ,@body))
@@ -46,25 +47,69 @@
    (oterm-wait-for-term-buffer-and-proc-to-die oterm-term-buffer oterm-term-proc 2)
    (should (string-suffix-p "finished\n" (buffer-substring-no-properties (point-min) (point-max))))))
 
-(defun oterm-wait-for-output ()
-  (unless (accept-process-output oterm-term-proc 0 500 t)
-    (error "no output"))
-  (while (accept-process-output oterm-term-proc 0 0 t)))
+(ert-deftest test-oterm-scroll-with-long-command ()
+  (with-oterm-buffer
+   (let ((loop-command "for i in {0..49}; do echo line $i; done"))
+     (oterm-send-raw-string loop-command)
+     (oterm-wait-for-output)
+     (should (equal (concat "$ " loop-command "<>") (oterm-test-content)))
+     (should (equal (mapconcat (lambda (i) (format "line %d\n" i)) (number-sequence 0 49) "")
+                    (oterm-run-and-capture-command-output))))))
 
-(defun oterm-test-content  (&optional nopointers)
+(ert-deftest test-oterm-scroll-with-many-commands ()
+  (with-oterm-buffer
+   (let ((loop-command "for i in {0..4}; do echo line $i; done"))
+     (dotimes (i 10)
+       (oterm-send-raw-string loop-command)
+       (oterm-wait-for-output)
+       (should (equal (mapconcat (lambda (i) (format "line %d\n" i)) (number-sequence 0 4) "")
+                      (oterm-run-and-capture-command-output)))))))
+
+(defun oterm-wait-for-output ()
+  "Wait for process output, which should be short and immediate."
+  (unless (accept-process-output oterm-term-proc 0 500 t)
+    (error "no output")))
+
+(defun oterm-run-and-capture-command-output ()
+  "Return everything between two prompts.
+
+This function sends RET to the process, then waits for the next
+prompt to appear. Once the prompt has appeared, it captures
+everything between the two prompts, return it, and narrow the
+buffer to a new region at the beginning of the new prompt."
+  (let ((first-prompt-end (point))
+        next-prompt-start output)
+    (oterm-send-input)
+    (while (not (save-excursion
+                  (goto-char first-prompt-end)
+                  (search-forward-regexp (concat "^" (regexp-quote oterm-test-prompt)) nil 'noerror)))
+      (unless (accept-process-output oterm-term-proc 1 nil t)
+        (error "no output")))
+    (setq next-prompt-start (match-beginning 0))
+    (setq output (oterm-test-content (1+ first-prompt-end) next-prompt-start))
+    (narrow-to-region next-prompt-start (point-max))
+    output))
+
+(defun oterm-test-content  (&optional start end nopointers)
   (interactive)
-  (let ((output (buffer-substring-no-properties (point-min) (point-max)))
-        (p (- (point) (point-min)))
-        (pmark (- (oterm--work-pmark) (point-min))))
+  (let* ((start (or start (point-min)))
+         (end (or end (point-max)))
+         (output (buffer-substring-no-properties start end))
+         (p (- (point) start))
+         (pmark (- (oterm--work-pmark) start))
+         (length (- end start)))
     (unless nopointers 
       (setq output
             (cond 
-             ((= p pmark)
-              (concat (substring output 0 p) "<>" (substring output p)))
-             ((> p pmark)
+             ((and (> p pmark) (>= p 0) (< p length) (>= pmark 0) (< pmark length))
               (concat (substring output 0 pmark) "<pmark>" (substring output pmark p) "<>" (substring output p)))
-             ((< p pmark)
-              (concat (substring output 0 p) "<>" (substring output p pmark) "<pmark>" (substring output pmark))))))
+             ((and (< p pmark) (>= p 0) (< p length) (>= pmark 0) (< pmark length))
+              (concat (substring output 0 p) "<>" (substring output p pmark) "<pmark>" (substring output pmark)))
+             ((and (>= p 0) (<= p length))
+              (concat (substring output 0 p) "<>" (substring output p)))
+             ((and (>= pmark 0) (<= pmark length))
+              (concat (substring output 0 p) "<pmark>" (substring output p)))
+             (t output))))
     (setq output (replace-regexp-in-string "\\$ \\(<>\\)?\n?$" "" output))
     (setq output (replace-regexp-in-string "[ \t]*$" "" output))
     output))

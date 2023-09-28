9b69ee309b762d7936e60cf47bf0f3f0c44d5ebd
Stephane Zermatten <szermatt@gmx.net>
2023-09-28 22:30:35 +0300

    Test yasnippet expansion with fish and insertions.

--- test/mistty-compat-test.el
+++ test/mistty-compat-test.el
@@ -93,3 +93,37 @@
                            "ok\n"
                            "$ <>")
                    (mistty-test-content :show (point))))))
+
+(ert-deftest mistty-compat-yas-expand-multiline-fish-insert ()
+  (skip-unless (featurep 'yasnippet))
+  (yas-define-snippets
+   'mistty-mode
+   ;; (KEY TEMPLATE NAME ...)
+   '(("fif" "if ${1}\n${0}\nend" "fif")))
+  (mistty-with-test-buffer (:shell fish :selected t)
+    (yas-minor-mode-on)
+    (keymap-local-set "C-c y" #'yas-expand)
+    (mistty-send-text "fif")
+    ;; yasnippet wants to delete the \n following fif. This is not
+    ;; possible and so causes a timeout.
+    (let ((mistty--report-issue-function nil))
+      (execute-kbd-macro (kbd "C-c y"))
+      (mistty-run-command
+       (insert "true"))
+      (execute-kbd-macro (kbd "TAB"))
+      (mistty-run-command
+       (insert "echo ok"))
+      (execute-kbd-macro (kbd "TAB"))
+      (mistty-wait-for-output :test (lambda () (not mistty--inhibit)))
+      (mistty-wait-for-output :test (lambda () (mistty--queue-empty-p mistty--queue))))
+    (should (equal (concat "$ if true\n"
+                           "      echo ok\n"
+                           "  end<>")
+                   (mistty-test-content :show (point))))
+    (mistty-send-and-wait-for-prompt)
+    (should (equal (concat "$ if true\n"
+                           "      echo ok\n"
+                           "  end\n"
+                           "ok\n"
+                           "$ <>")
+                   (mistty-test-content :show (point))))))

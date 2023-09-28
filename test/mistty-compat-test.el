906a6d9a9f1d724689dea778c277329e1ab5cd9c
Stephane Zermatten <szermatt@gmx.net>
2023-09-28 22:23:28 +0300

    Test yasnippet multiline expansion in fish.
    The main difficulty with fish is dealing with indentation being added to
    the text during the replay.

--- test/mistty-compat-test.el
+++ test/mistty-compat-test.el
@@ -65,3 +65,31 @@
     (should (equal "$ if true\nthen echo ok\nfi<>"
                    (mistty-test-content :show (point))))
     (should (equal "ok" (mistty-send-and-capture-command-output)))))
+
+(ert-deftest mistty-compat-yas-expand-multiline-fish ()
+  (skip-unless (featurep 'yasnippet))
+  (yas-define-snippets
+   'mistty-mode
+   ;; (KEY TEMPLATE NAME ...)
+   '(("fif" "if ${1}\n${0}\nend" "fif")))
+  (mistty-with-test-buffer (:shell fish :selected t)
+    (yas-minor-mode-on)
+    (keymap-local-set "C-c y" #'yas-expand)
+    (mistty-send-text "fif")
+    ;; yasnippet wants to delete the \n following fif. This is not
+    ;; possible and so causes a timeout.
+    (let ((mistty--report-issue-function nil))
+      (execute-kbd-macro (kbd "C-c y t r u e TAB e c h o SPC o k TAB"))
+      (mistty-wait-for-output :test (lambda () (not mistty--inhibit)))
+      (mistty-wait-for-output :test (lambda () (mistty--queue-empty-p mistty--queue))))
+    (should (equal (concat "$ if true\n"
+                           "      echo ok\n"
+                           "  end<>")
+                   (mistty-test-content :show (point))))
+    (mistty-send-and-wait-for-prompt)
+    (should (equal (concat "$ if true\n"
+                           "      echo ok\n"
+                           "  end\n"
+                           "ok\n"
+                           "$ <>")
+                   (mistty-test-content :show (point))))))

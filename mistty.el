eb857e18477d81c3d42a403c5d45b3953d04e556
Stephane Zermatten <szermatt@gmx.net>
2023-11-20 18:43:14 +0200

    Revert "Release mistty 1.0"
    This reverts commit 2cd10be10cfd19c327f02368eede1104a8c68cd8.

--- mistty.el
+++ mistty.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2023 Stephane Zermatten
 
 ;; Author: Stephane Zermatten <szermatt@gmx.net>
-;; Version: 1.0
+;; Version: 0.9
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: convenience, unix
 ;; URL: http://github.com/szermatt/mistty

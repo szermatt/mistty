f70ea7f5c91f5df6ac0d3a09eaccc283fbdbce70
Stephane Zermatten <szermatt@gmx.net>
2024-11-23 20:03:21 +0200

    Release mistty 1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'MisTTY'
 copyright = '2023-2024, Stephane Zermatten'
 author = 'Stephane Zermatten'
 
-release = '1.1.1snapshot'
-version = '1.1.1snapshot'
+release = '1.2'
+version = '1.2'
 
 # -- General configuration
 

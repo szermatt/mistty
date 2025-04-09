4ee613e1c86a38e470938736b8dc04684b83502e
Stephane Zermatten <szermatt@gmx.net>
2025-04-10 00:24:54 +0300

    chore: de-linted mistty-launch.el

--- mistty-launch.el
+++ mistty-launch.el
@@ -1,6 +1,27 @@
 ;;; mistty-launch.el --- Convenience TRAMP-based launcher for MisTTY -*- lexical-binding: t -*-
 
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; `http://www.gnu.org/licenses/'.
+
+;;; Commentary:
+;;
+;; This file defines convenience functions for starting mistty buffers
+;; with TRAMP.
+
 (require 'pcmpl-unix)
+
+;;; Code:
 
 (defvar mistty-ssh-history nil
   "History variable for host completion for `mistty-ssh'.")
@@ -17,13 +38,24 @@ This is used by `mistty-docker'")
 (defun mistty-ssh (&optional host command other-window)
   "Open SSH running on HOST in a MisTTY buffer.
 
-This is a convenience function that uses TRAMP to connect to a
-host and open a shell to it. For more control, run
-C-u \\[mistty-create]. That'll allow you to setup any TRAMP
-remote path you'd like without restrictions.
+This is a convenience function that uses TRAMP to connect to HOST and
+executor COMMAND.
+
+For more control, run \\[mistty-create] with an ; argument. That'll
+allow you to setup any TRAMP remote path you'd like without
+restrictions.
 
 See Info Node `(mistty)Remote Shells with TRAMP' for more
-details."
+details.
+
+If OTHER-WINDOW is nil, execute the default action configured by
+`display-comint-buffer-action'. If OTHER-WINDOW is a function, it is
+passed to `pop-to-buffer` to be used as a `display-buffer' action. With
+any other non-nil value, display the buffer in another window.
+
+    You might prefer configuring `display-buffer-alist' for
+    comint category buffers to get the exact behavior you want instead
+    of passing OTHER-WINDOW."
   (interactive
    (list
     (completing-read "Host: " (pcmpl-ssh-hosts)
@@ -36,19 +68,30 @@ details."
 (defun mistty-docker (&optional instance command other-window)
   "Connect to a docker instance in a MisTTY buffer.
 
-This is a convenience function that uses TRAMP to connect to a
-host and open a shell to it. For more control, run
-C-u \\[mistty-create]. That'll allow you to setup any TRAMP
-remote path you'd like without restrictions.
+This connects to docker INSTANCE and executo COMMAND.
+
+This is a convenience function that uses TRAMP to connect to a host and
+open a shell to it. For more control, run \\[mistty-create] with an
+argument. That'll allow you to setup any TRAMP remote path you'd like
+without restrictions.
 
 See Info Node `(mistty)Remote Shells with TRAMP' for details.
 
-Note that docker instances often run older versions of Bash;
-MisTTY will work on Bash versions < 5.1, with some limitations.
-Additionally, for Bash 4.3 and earlier, you might have to set
-`mistty-set-EMACS' to non-nil for directory tracking to work. You
-can set it per instance as a connection-local variable. See Info
-Anchor `(mistty)shells bash-dirtrack' for details."
+Note that docker instances often run older versions of Bash; MisTTY will
+work on Bash versions < 5.1, with some limitations. Additionally, for
+Bash 4.3 and earlier, you might have to set `mistty-set-EMACS' to
+non-nil for directory tracking to work. You can set it per instance as a
+connection-local variable. See Info Anchor `(mistty)shells
+bash-dirtrack' for details.
+
+If OTHER-WINDOW is nil, execute the default action configured by
+`display-comint-buffer-action'. If OTHER-WINDOW is a function, it is
+passed to `pop-to-buffer` to be used as a `display-buffer' action. With
+any other non-nil value, display the buffer in another window.
+
+    You might prefer configuring `display-buffer-alist' for comint
+    category buffers to get the exact behavior you want instead of
+    passing OTHER-WINDOW."
   (interactive
    (list
     (completing-read
@@ -61,3 +104,7 @@ Anchor `(mistty)shells bash-dirtrack' for details."
   (let ((default-directory (format "/docker:%s:~" instance)))
     (cd default-directory) ;; Connect, ask password
     (mistty-create command other-window)))
+
+(provide 'mistty-launch)
+
+;;; mistty-launch.el ends here

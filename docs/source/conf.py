56110c29574d19732435294bafc22194aa73f80c
Stephane Zermatten <szermatt@gmx.net>
2026-09-16 11:57:57 +0300

    chore: Development version, following release 2.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'MisTTY'
 copyright = '2023-2026, Stephane Zermatten'
 author = 'Stephane Zermatten'
 
-release = '2.0.1'
-version = '2.0.1'
+release = '2.0.2snapshot'
+version = '2.0.2snapshot'
 
 # -- General configuration
 
--- mistty.el
+++ mistty.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2023-2026 Stephane Zermatten
 
 ;; Author: Stephane Zermatten <szermatt@gmx.net>
-;; Version: 2.0.1
+;; Version: 2.0.2snapshot
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: convenience, unix
 ;; URL: http://github.com/szermatt/mistty
